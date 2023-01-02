4a3483ae5f22ce2aa387b4c42fcdee4b5a38265f
Tristan Dubé-Lepage <tristan.dube@gmail.com>
2023-01-02 16:16:07 -0500

    Some prototypes command
    Added a bunch of stuff to try and test the issue with the debugger.

--- src/kernel/.gdbinit
+++ src/kernel/.gdbinit
@@ -1,4 +1,25 @@
+# Shortcut for Layout Next
+define ln
+    layout next
+end
+
+# Shortcut for Focus Next
+define fn
+    focus next
+end
+
+# Kill current program and exit debugger
+define killq
+    kill
+    q
+end
+
+set debug aarch64
+show debug aarch64
 target remote localhost:1234
 symbol-file out/myos.elf
 tui enable
+layout regs
+tui reg general
 break kernel_main
+break drop_from_el3
--- src/kernel/boot.s
+++ src/kernel/boot.s
@@ -12,6 +12,7 @@ _start:
     b       1b
 2:  // cpu id == 0
 
+set_stack:
     // set top of stack just before our code (stack grows to a lower address per AAPCS64)
     ldr     x1, =_start
     mov     sp, x1
@@ -28,3 +29,23 @@ _start:
 4:  bl      kernel_main
     // for failsafe, halt this core too
     b       1b
+
+drop_from_el3:
+    adr x0, el2_start   // Read address of el2_start into x0
+    msr ELR_EL3, x0     // Load start addr in the ELR register
+
+    mov x0, 0b1001      // Set bits 0 and 3
+    msr SPSR_EL3, x0    // Set SPSR to indicate using SP stored in SP_EL2
+
+    mov x0, sp          // Store out current stack pointer in x0
+    msr SP_EL2, x0      // Point EL2's SP to our current EL3 SP
+
+    eret                // Return out of EL3 into EL2
+
+el2_start:
+    //mrs x0, CurrentEL
+    //bl kernel_main
+    bl test_test
+
+test_test:
+    ldr x0, 0
