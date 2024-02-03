97cf4375053a4aee6d43210177fb0a7e853b02f7
Tristan Dubé-Lepage <tristan.dube@gmail.com>
2024-02-03 10:40:37 -0500

    Switch back to Raspi3b
    When using the virt board it is hard to emulate specific additional
    devices. It basically tries to emulate a Linux PC. Stuff like a UART
    is not present.
    
    I got inspired by some code online that ran a specific machine + a
    specific CPU. This gave me the idea of running a Raspi3 with an A76.
    
    Since devices should be fairly similar between RPis this should do for
    now.

--- src/kernel/kernel.c
+++ src/kernel/kernel.c
@@ -5,8 +5,17 @@
 #include "types.h"
 #include "uart.h"
 
+int test(int a, int b, char* test) {
+    int c = a + b;
+
+    return c;
+}
+
 // arguments for AArch64
 void kernel_main(uint64_t dtb_ptr32, uint64_t x1, uint64_t x2, uint64_t x3) {
+
+    int res = test(1, 2, "test");
+
     // initialize UART for Raspi2
     uart_init(3);
     uart_puts("Hello, kernel World!\r\n");
